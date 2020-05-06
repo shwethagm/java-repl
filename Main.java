5c98a0d9109efdfbf0cf5b2a928b962bd65fe4e5
shwethagm <Shwethagm9999@gmail.com>
2020-05-06 14:18:44 +0000

    Implemented Method Part3 1 program

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("my java tutorial programs !");
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Enter the number of times the program to be printed");
+int num=Integer.valueOf(scanner.nextLine());
+Part3Method1 p1=new Part3Method1();
+int i=0;
+while(i<num){
+ p1. printText();
+i++;
+}
+    
+   
     //ExitingLoop el=new ExitingLoop();
     //el.exitLoop();
     //PositiveNumber pum=new PositiveNumber();
@@ -18,9 +30,9 @@ class Main {
     //pn.getNumber();
     //SumOfSequence ss=new SumOfSequence();
     //ss.getSumS();
-    LargeProgram lp=new LargeProgram();
-    lp.getNumber();
-    // Factorial ff=new Factorial(); 
+    // LargeProgram lp=new LargeProgram();
+    // lp.getNumber();
+    // // Factorial ff=new Factorial(); 
     // ff.getFactorial();   //an.getAverage();
     //an.getAVerageOfPositive();
     //AverageCalculator avg=new AverageCalculator();
---
+++ Part3Method1.java
@@ -0,0 +1,7 @@
+ class Part3Method1{
+
+public static void printText() {
+    // Write some code in here
+    System.out.println("In a hole in the ground there lived a method");
+}
+ }
